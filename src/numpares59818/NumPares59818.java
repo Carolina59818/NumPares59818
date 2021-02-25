28b62aa65357b2c746f2eb1868385e9b44258306
caroc <caroc@192.168.1.71>
2021-02-24 20:56:49 -0600

    Primeros 47 numeros pares

---
+++ src/numpares59818/NumPares59818.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numpares59818;
+
+/**
+ *
+ * @author caroc
+ */
+public class NumPares59818 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Hacer un proyecto que imprima los primeros 47 números pares.
+      int numero = 0 ;
+      while (numero < 94) {
+          if (numero%2==0) {
+              System.out.println("numero = " + numero);
+          }
+              numero++; 
+          }
+      }
+     
+      
+    }
+    
+}
